3da78d8bafdde879e65674cf74739639c18f0a60
Alecs Geuder <al.geuder@gmail.com>
2015-02-19 15:55:57 +0000

    freezing multiboard
    * see commit of similar name in mainSketch simplifiedLayoutChange
      branch

--- yunTransmitter.h
+++ yunTransmitter.h
@@ -9,20 +9,28 @@
 #define BYTESUM_CHARLENGTH 6
 #define CHECKSUM_LENGTH TIMESTAMP_LENGTH + BYTESUM_CHARLENGTH
 
-#define BUFFER_LENGTH 23 * 5
-
 #define CHECK_FOR_UPDATES_DURATION 5000
 
 #define COLLECTOR 0x1
 #define STORAGE 0x2
 
+#define CLOSE_DATAPOINT ')'
+
 #define TIMESTAMP_LENGTH 10
 #define TS_INDEX_DP 3
 
+#define DATAPOINT_MAX 5
+#define MAX_VALUE_LENGTH 10
+#define MAX_DATAPOINT_LENGTH MAX_VALUE_LENGTH + TIMESTAMP_LENGTH + 3
+#define DSR_BUFFER_LENGTH MAX_DATAPOINT_LENGTH * DATAPOINT_MAX
+
 #define NO_DATA_CHAR '!'
 
-byte server[] = {10, 10, 63, 133};  // WIFI-specifics
+byte server[] = {10, 10, 63, 175};  // WIFI-specifics
 //byte server[] = {192, 168, 0, 7};  // WIFI-specifics
+
+YunClient eClient;
+
 
 bool requestToDelete(byte* checksum, unsigned int length) {
   char* checkSum = new char[length + 1];
@@ -46,51 +54,28 @@ void callback(char* topic, byte* payload, unsigned int length) {
   }
 }
 
+PubSubClient client(server, 1883, callback, eClient);
+
 int freERam () {
   extern int __heap_start, *__brkval;
   int v;
   return (int) &v - (__brkval == 0 ? (int) &__heap_start : (int) __brkval);
 }
 
-YunClient eClient;
-PubSubClient client(server, 1883, callback, eClient);
-
-
-void buildChecksum(char* checksum, char* buffer) {
-  unsigned long checksumByteSum = 0; // sum of all byte-values of the file
-
-  char ts[TIMESTAMP_LENGTH + 1];
-  int i = 0;
-  for (i;i<strlen(buffer);i++) {
-    if (i>=TS_INDEX_DP && i<TS_INDEX_DP+TIMESTAMP_LENGTH) {
-      ts[i-TS_INDEX_DP] = buffer[i];
-    } else if (i == TS_INDEX_DP+TIMESTAMP_LENGTH) {
-      ts[i-TS_INDEX_DP] = '\0';
-    }
-    checksumByteSum += (byte)buffer[i];
-  }
-  //Serial.println("checksumByteSum: " + String(checksumByteSum));
-  sprintf(checksum, "%s:%ld", ts, checksumByteSum);
+void reportFreeRam() {
+  Serial.println("Free Ram : " + String(freERam()));
 }
 
-bool requestDataForSending(char* sendBuffer, char* checksum) {
-  bool gotDataPoint;
-  Wire.requestFrom(STORAGE, strlen(sendBuffer));
-  byte i = 0;
-  while (Wire.available()) {
-    char c = Wire.read();
-    if (c == NO_DATA_CHAR) {
-      gotDataPoint = false;
-      continue;
-    }
-    sendBuffer[i] = c;
-    i++;
+
+
+unsigned long buildChecksum(byte i, char c, char* ts, unsigned long checksumByteSum) {
+  if (i>=TS_INDEX_DP && i<TS_INDEX_DP+TIMESTAMP_LENGTH) {
+    ts[i-TS_INDEX_DP] = c;
+  } else if (i == TS_INDEX_DP+TIMESTAMP_LENGTH) {
+    ts[i-TS_INDEX_DP] = '\0';
   }
-  if (gotDataPoint) {
-    buildChecksum(checksum, sendBuffer);
-  }
-  Serial.println("T: checksum: " + String(checksum));
-  return gotDataPoint;
+  checksumByteSum += (byte)c;
+  return checksumByteSum;
 }
 
 void checkForUpdates() {
@@ -99,25 +84,71 @@ void checkForUpdates() {
   while (millis() - start <= CHECK_FOR_UPDATES_DURATION) {
     if (client.loop()) break;
   }
+  Serial.println("T: no updates found.");
   client.disconnect();
 }
 
-// 3. Data Transfer to server
-void sendData() {
-  if (client.connected() || client.connect("siteX", "punterX", "punterX")) {
-    Serial.println("T: connected!");
-    char sendBuffer[BUFFER_LENGTH + 1];
-    char checksum[CHECKSUM_LENGTH + 1];
-    if (requestDataForSending(sendBuffer, checksum)) {
-      //Serial.println("[T:] trying to send it..");
-      if (client.publish(checksum, sendBuffer)) {
-        //Serial.println("[T:] should have sent the stuff by now..");
-      }
-    } else {
-      //Serial.println("[T:] No data for sending available!");
-    }
-    checkForUpdates();
-  } else {
-    //Serial.println("[T:] Didn't get a connection!");
+void sendData(char* sendBuffer, char* checksum) {
+  Serial.println("Trying to send " + String(sendBuffer));
+  Serial.println(checksum);
+
   }
+
+// TODO: namespace global vars properly!!
+char sendBuffer[DSR_BUFFER_LENGTH + 1];
+byte sendBufferIndex;
+// checksum related vars
+char ts[TIMESTAMP_LENGTH + 1];      // the first timestamp in the data
+unsigned long checksumByteSum = 0;  // sum of all byte-values of the file
+char checksum[CHECKSUM_LENGTH + 1]; // write the finished checksum here
+bool dataReady = false;             //
+
+void retrieveDataForSending(int dataSize) {
+  if (dataReady) {
+    Serial.println("still data to send");
+    return;
+  }
+  while (Wire.available() > 0) {
+    char c = Wire.read();
+    Serial.print(c);
+    sendBuffer[sendBufferIndex] = c;
+    checksumByteSum = buildChecksum(sendBufferIndex, c, ts, checksumByteSum);
+    if (c == NO_DATA_CHAR) {
+      sendBuffer[sendBufferIndex] = '\0';
+      Serial.println("T: checksumByteSum: " + String(checksumByteSum));
+      sprintf(checksum, "%s:%ld", ts, checksumByteSum);
+      dataReady = true;
+      break;
+    }
+    sendBufferIndex++;
+  }
+}
+void resetData(){
+  sendBuffer[0] = '\0';
+  sendBufferIndex = 0;
+  ts[0] = '\0';
+  checksumByteSum = 0;
+  checksum[0] = '\0';
+  dataReady = false;
+}
+bool transmitting = false;
+void sendToServer(){
+  Serial.println("hello");
+  reportFreeRam();
+  Serial.println(client.connected());
+  Serial.println(client.connect("siteX", "punterX", "punterX"));
+  // NOTE: the following should run (as tested in the two prev. lines but doesn't,
+  // no idea why..
+  //if (client.connected() || client.connect("siteX", "punterX", "punterX")) {
+  //  transmitting = true;
+  //  Serial.println("T: connected!");
+  //  if (client.publish(checksum, sendBuffer)) {
+  //    resetData();
+  //    Serial.println("T: should have sent the stuff by now..");
+  //  }
+  //  checkForUpdates();
+  //  transmitting = false;
+  //} else {
+  //  Serial.println("T: Didn't get a connection!");
+  //}
 }
